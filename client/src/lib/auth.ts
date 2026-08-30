160891a9456ef35709d5b056147eb28b3a6a0ec3
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-08-30 21:58:44 +0600

    feat: integrate Google icon in login button and update auth configuration

--- client/src/lib/auth.ts
+++ client/src/lib/auth.ts
@@ -7,10 +7,12 @@ const client = new MongoClient(process.env.MONGODB_URI as string);
 const db = client.db(process.env.DB_NAME as string);
 
 export const auth = betterAuth({
-  trustedOrigins: [
-    process.env.BETTER_AUTH_URL || "http://localhost:3000",
-  ],
 
+  secret: process.env.BETTER_AUTH_SECRET,
+  advanced: {
+    cookiePrefix: "my_app_v2"
+  },
+ 
   emailAndPassword: {
     enabled: true,
     sendResetPassword: async ({ user, url }) => {
